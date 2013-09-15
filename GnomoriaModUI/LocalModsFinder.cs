5ae3d2c0df55d03afb7b96c2a1ee22a7cea2204a
AndreasKarg <Clonkman@gmx.de>
2013-09-15 09:12:51 +0200

    Cleanup: Superficial refactoring of LocalModsFinder.cs

--- GnomoriaModUI/LocalModsFinder.cs
+++ GnomoriaModUI/LocalModsFinder.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Linq;
-using System.Text;
 using System.Reflection;
 using Faark.Gnomoria.Modding;
 using Faark.Util;
@@ -19,6 +18,7 @@ namespace GnomoriaModUI
         {
             OnDependencyFound.TryRaise(this, mod);
         }
+
         public event EventHandler<EventArgs<IMod>> OnModFound;
         public virtual void ModFound(IMod mod)
         {
@@ -26,37 +26,39 @@ namespace GnomoriaModUI
         }
         public event EventHandler OnSearchEnded;
 
-        private List<IMod> foundDependencies = new List<IMod>();
-        private List<IMod> foundMods = new List<IMod>();
+        private readonly List<IMod> _foundDependencies = new List<IMod>();
+        private readonly List<IMod> _foundMods = new List<IMod>();
 
         public virtual void SearchEnded()
         {
             OnSearchEnded.TryRaise(this);
         }
-        private void processModType(ModType modType, bool isDep = false)
+
+        private void ProcessModType(ModType modType, bool isDep = false)
         {
             try
             {
                 var mod = ModEnvironment.Mods[modType];
                 if (isDep)
                 {
-                    foundDependencies.Add(mod);
+                    _foundDependencies.Add(mod);
                 }
                 else
                 {
-                    foundMods.Add(mod);
+                    _foundMods.Add(mod);
                 }
                 mod.Initialize_ModDiscovery();
+
                 foreach (var dep in mod.Dependencies)
                 {
-                    if (foundMods.Count(fmod => fmod.GetType() == dep.Type) <= 0)
+                    if (_foundMods.Count(fmod => fmod.GetType() == dep.Type) > 0) continue;
+
+                    if (_foundDependencies.Count(fdep => fdep.GetType() == dep.Type) <= 0)
                     {
-                        if (foundDependencies.Count(fdep => fdep.GetType() == dep.Type) <= 0)
-                        {
-                            processModType(dep, true);
-                        }
+                        ProcessModType(dep, true);
                     }
                 }
+
                 if (isDep)
                 {
                     DependencyFound(mod);
@@ -72,7 +74,7 @@ namespace GnomoriaModUI
                 //to log...
             }
         }
-        public void RunSync(System.IO.DirectoryInfo gnomoria_base)
+        public void RunSync(System.IO.DirectoryInfo gnomoriaBase)
         {
             try
             {
@@ -81,18 +83,20 @@ namespace GnomoriaModUI
                 //processModType(typeof(Faark.Gnomoria.Modding.DemoMods.Game_CreateBackupSavegame));
 
 
-                var mod_dir = gnomoria_base.GetDirectories().Single(sub => sub.Name.ToUpper() == "MODS");
-                var mod_files = mod_dir.GetFiles("*.dll");
-                var mod_assemblies = new List<Assembly>();
-                foreach (var mod_file in mod_files)
+                var modDir = gnomoriaBase.GetDirectories().Single(sub => sub.Name.ToUpper() == "MODS");
+                var modFiles = modDir.GetFiles("*.dll");
+                var modAssemblies = new List<Assembly>();
+
+                foreach (var modFile in modFiles)
                 {
-                    var uri = new Uri(mod_file.FullName).AbsoluteUri;
+                    var uri = new Uri(modFile.FullName).AbsoluteUri;
                     var zone = System.Security.Policy.Zone.CreateFromUrl(uri);
                     //MessageBox.Show(zone.SecurityZone + "\n" + uri);
                     var maySecurityProblems = zone.SecurityZone == System.Security.SecurityZone.Internet || zone.SecurityZone == System.Security.SecurityZone.Untrusted;
+
                     try
                     {
-                        mod_assemblies.Add(Assembly.LoadFrom(mod_file.FullName));
+                        modAssemblies.Add(Assembly.LoadFrom(modFile.FullName));
                     }
                     catch (BadImageFormatException)
                     {
@@ -100,27 +104,28 @@ namespace GnomoriaModUI
                     }
                     catch (Exception err)
                     {
-                        var msg = "Error loading file: "+mod_file.FullName+"\n\n";
+                        var msg = "Error loading file: "+modFile.FullName+"\n\n";
                         if (maySecurityProblems)
                         {
                             msg += "This file could be blocked due to security stuff. Try right click it in explorer to open its properties. Check the general tab for 'Security' at the bottom.\n\n\n";
                         }
                         msg += err.ToString();
-                        System.Windows.Forms.MessageBox.Show(msg);
+                        MessageBox.Show(msg);
                     }
                 }
-                var modTypes = new List<Type>();//todo: place this somewhere else?
-                foreach (var assembly in mod_assemblies)
+
+                foreach (var assembly in modAssemblies)
                 {
                     try
                     {
-                        var mod_types = assembly.GetTypes().Where(t =>
+                        var modTypes = assembly.GetTypes().Where(t =>
                         {
-                            return typeof(IMod).IsAssignableFrom(t) && !(typeof(Faark.Gnomoria.Modding.SupportMod).IsAssignableFrom(t));
+                            return typeof(IMod).IsAssignableFrom(t) && !(typeof(SupportMod).IsAssignableFrom(t));
                         });
-                        foreach (var mod_type in mod_types)
+
+                        foreach (var modType in modTypes)
                         {
-                            processModType(new ModType(mod_type));
+                            ProcessModType(new ModType(modType));
                         }
                     }
                     catch (ReflectionTypeLoadException)
@@ -129,17 +134,18 @@ namespace GnomoriaModUI
                     }
                     catch (Exception err)
                     {
-                        System.Windows.Forms.MessageBox.Show(err.ToString());
+                        MessageBox.Show(err.ToString());
                     }
                 }
             }
             catch (Exception err)
             {
-                System.Windows.Forms.MessageBox.Show(err.ToString());
+                MessageBox.Show(err.ToString());
             }
 
             SearchEnded();
         }
+
         public void RunAsync()
         {
             throw new NotImplementedException();
