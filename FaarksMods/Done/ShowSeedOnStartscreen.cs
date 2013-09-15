ad92d2a5b22beac3b63407a574b8a2c262e85e5a
AndreasKarg <Clonkman@gmx.de>
2013-09-15 12:33:02 +0200

    Cleanup: Superficial refactoring of ShowSeedOnStartscreen.cs

--- FaarksMods/Done/ShowSeedOnStartscreen.cs
+++ FaarksMods/Done/ShowSeedOnStartscreen.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
 using System.Reflection;
 using Faark.Gnomoria.Modding;
-using Faark.Util;
 
 namespace Faark.Gnomoria.Mods
 {
@@ -19,7 +18,7 @@ namespace Faark.Gnomoria.Mods
             {
                 return new IModification[]{
                     new MethodHook(
-                        typeof(Game.GUI.MainMenuWindow).GetConstructor(new Type[] { typeof(Game.GUI.Controls.Manager) }),
+                        typeof(Game.GUI.MainMenuWindow).GetConstructor(new[] { typeof(Game.GUI.Controls.Manager) }),
                         Method.Of<Game.GUI.MainMenuWindow, Game.GUI.Controls.Manager>(OnAfter_MainMenuWindow_Created)
                         ),
                     new MethodHook(
@@ -29,6 +28,7 @@ namespace Faark.Gnomoria.Mods
                 };
             }
         }
+
         public override string Author
         {
             get
@@ -36,93 +36,107 @@ namespace Faark.Gnomoria.Mods
                 return "Faark";
             }
         }
+
         public override string Description
         {
             get
             {
-                return "Displays the seed for that automatically generated world on the start screen. You can click it to create a new world with that seed.";
+                return
+                    "Displays the seed for that automatically generated world on the start screen. You can click it to create a new world with that seed.";
             }
         }
 
+        private static string _seed;
+        private static Game.GUI.Controls.Label _versionLabel;
+        private static Game.GUI.Controls.Label _seedLabel;
+        private static uint _lastSeed;
 
-        private static string seed = null;
-        private static Game.GUI.Controls.Label version_label = null;
-        private static Game.GUI.Controls.Label seed_label = null;
-        private static uint last_seed;
         public static void OnAfter_MainMenuWindow_Created(Game.GUI.MainMenuWindow self, Game.GUI.Controls.Manager mgr)
         {
-            seed_label = null;
-            version_label = (Game.GUI.Controls.Label)typeof(Game.GUI.MainMenuWindow)
+            _seedLabel = null;
+            _versionLabel = (Game.GUI.Controls.Label)typeof(Game.GUI.MainMenuWindow)
                 .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
                 .Single(field => field.FieldType == typeof(Game.GUI.Controls.Label))
                 .GetValue(self);
             TryLabelUpdate();
         }
+
         public static void OnAfter_PreviewMap(Game.GnomanEmpire self, Game.CreateWorldOptions worldOptions, bool clear, int xyScale)
         {
             var task = (System.Threading.Tasks.Task)typeof(Game.GnomanEmpire)
                 .GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static)
                 .Single(field => field.FieldType == typeof(System.Threading.Tasks.Task))
                 .GetValue(self);
-            task.ContinueWith(new Action<System.Threading.Tasks.Task>((t) =>
+            task.ContinueWith(t =>
             {
-                seed = (last_seed = self.Map.WorldSeed).ToString();
+                _seed = (_lastSeed = self.Map.WorldSeed).ToString(CultureInfo.InvariantCulture);
                 TryLabelUpdate();
-            }));
+            });
         }
+
         private static void TryLabelUpdate()
         {
-            if ((seed != null) && (version_label != null))
+            if ((_seed == null) || (_versionLabel == null)) return;
+
+            int right;
+
+            if (_seedLabel == null)
             {
-                int right;
-                if (seed_label == null)
-                {
-                    seed_label = new Game.GUI.Controls.Label(version_label.Manager);
-                    seed_label.Init();
-                    seed_label.Anchor = Game.GUI.Controls.Anchors.Left | Game.GUI.Controls.Anchors.Bottom;
-                    seed_label.Top = version_label.Top;
-                    var default_color = seed_label.TextColor = version_label.TextColor;
-                    seed_label.ToolTip = new Game.GUI.Controls.ToolTip(version_label.Manager) { Text = "Create a new world with this Seed." };
-                    seed_label.Passive = false;
-                    seed_label.CanFocus = true;
-                    /*seed_label.MouseMove += new Game.GUI.Controls.MouseEventHandler((sender, args) =>
+                _seedLabel = new Game.GUI.Controls.Label(_versionLabel.Manager);
+                _seedLabel.Init();
+                _seedLabel.Anchor = Game.GUI.Controls.Anchors.Left | Game.GUI.Controls.Anchors.Bottom;
+                _seedLabel.Top = _versionLabel.Top;
+                var defaultColor = _seedLabel.TextColor = _versionLabel.TextColor;
+                _seedLabel.ToolTip = new Game.GUI.Controls.ToolTip(_versionLabel.Manager) { Text = "Create a new world with this Seed." };
+                _seedLabel.Passive = false;
+                _seedLabel.CanFocus = true;
+                /*seed_label.MouseMove += new Game.GUI.Controls.MouseEventHandler((sender, args) =>
                     {
                         (sender as Game.GUI.Controls.Label).TextColor = Microsoft.Xna.Framework.Color.LightGreen;
                     });*/
-                    seed_label.MouseOver += new Game.GUI.Controls.MouseEventHandler((sender, args) =>
-                    {
-                        (sender as Game.GUI.Controls.Label).TextColor = Microsoft.Xna.Framework.Color.LightGreen;
-                    });
-                    seed_label.MouseOut += new Game.GUI.Controls.MouseEventHandler((sender, args) =>
-                    {
-                        (sender as Game.GUI.Controls.Label).TextColor = default_color;
-                    });
-                    seed_label.Click += new Game.GUI.Controls.EventHandler((sender, args) =>
-                    {
-                        Game.GnomanEmpire.Instance.GuiManager.MenuStack.PushWindow(
-                            new Game.GUI.AdvancedSetupWindow(
-                                ((Game.GUI.Controls.Control)sender).Manager, new Game.CreateWorldOptions() { 
-                                    Seed = last_seed,
-                                    KingdomName = Game.GnomanEmpire.Instance.World.LanguageManager.RandomFactionName(Game.GnomanEmpire.Instance.World.AIDirector.FactionDefs[0].Language)
-                                })
-                            );
-                    });
-                    version_label.Parent.Add(seed_label);
-                    right = version_label.Left + version_label.Width;
-                    version_label.Text += "; ";
-                    version_label.Width = (int)version_label.Skin.Layers[0].Text.Font.Resource.MeasureString(version_label.Text).X + 2;
-                }
-                else
+                _seedLabel.MouseOver += (sender, args) =>
                 {
-                    right = seed_label.Left + seed_label.Width;
-                }
-                seed_label.Text = seed.ToString();
-                seed_label.Width = (int)seed_label.Skin.Layers[0].Text.Font.Resource.MeasureString(seed_label.Text).X + 2;
-                seed_label.Left = right - seed_label.Width;
-                version_label.Left = seed_label.Left - version_label.Width;
-                seed = null;
-                version_label = null;
+                    var label = sender as Game.GUI.Controls.Label;
+                    if (label != null)
+                        label.TextColor = Microsoft.Xna.Framework.Color.LightGreen;
+                };
+
+                _seedLabel.MouseOut += (sender, args) =>
+                {
+                    var label = sender as Game.GUI.Controls.Label;
+                    if (label != null)
+                        label.TextColor = defaultColor;
+                };
+
+                // Todo: Cleanup
+                _seedLabel.Click += (sender, args) =>
+                {
+                    Game.GnomanEmpire.Instance.GuiManager.MenuStack.PushWindow(
+                        new Game.GUI.AdvancedSetupWindow(
+                            ((Game.GUI.Controls.Control)sender).Manager, new Game.CreateWorldOptions
+                            { 
+                                Seed = _lastSeed,
+                                KingdomName = Game.GnomanEmpire.Instance.World.LanguageManager.RandomFactionName(Game.GnomanEmpire.Instance.World.AIDirector.FactionDefs[0].Language)
+                            })
+                        );
+                };
+
+                _versionLabel.Parent.Add(_seedLabel);
+                right = _versionLabel.Left + _versionLabel.Width;
+                _versionLabel.Text += "; ";
+                _versionLabel.Width = (int)_versionLabel.Skin.Layers[0].Text.Font.Resource.MeasureString(_versionLabel.Text).X + 2;
             }
+            else
+            {
+                right = _seedLabel.Left + _seedLabel.Width;
+            }
+
+            _seedLabel.Text = _seed;
+            _seedLabel.Width = (int)_seedLabel.Skin.Layers[0].Text.Font.Resource.MeasureString(_seedLabel.Text).X + 2;
+            _seedLabel.Left = right - _seedLabel.Width;
+            _versionLabel.Left = _seedLabel.Left - _versionLabel.Width;
+            _seed = null;
+            _versionLabel = null;
         }
     }
 
