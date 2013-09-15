937c3af93fc59162e92c7ac340e552e27a7279e3
AndreasKarg <Clonkman@gmx.de>
2013-09-15 12:22:35 +0200

    Cleanup: Superficial refactoring of NextCharButtonInCharWindow.cs

--- FaarksMods/Done/NextCharButtonInCharWindow.cs
+++ FaarksMods/Done/NextCharButtonInCharWindow.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Reflection;
 using Faark.Gnomoria.Modding;
 using Game;
 using Game.GUI;
 using Game.GUI.Controls;
-using GameLibrary;
-using Faark.Util;
-using Microsoft.Xna.Framework;
 
 namespace Faark.Gnomoria.Mods
 {
@@ -25,17 +21,19 @@ namespace Faark.Gnomoria.Mods
             get
             {
                 yield return new MethodHook(
-                    typeof(ViewCharacterUI).GetConstructor(new Type[] { typeof(Manager), typeof(Character) }),
+                    typeof(ViewCharacterUI).GetConstructor(new[] { typeof(Manager), typeof(Character) }),
                     Method.Of<ViewCharacterUI, Manager, Character>(OnCreate_ViewCharacterUI)
                     );
             }
         }
 
-        private static FieldInfo TabbedWindow_TabControl_FieldInfo;
+        private static FieldInfo _tabbedWindowTabControlFieldInfo;
+
         public override void Initialize_PreGame()
         {
-            TabbedWindow_TabControl_FieldInfo = typeof(TabbedWindow).GetFields(BindingFlags.Instance | BindingFlags.NonPublic).Single(f => f.FieldType == typeof(TabControl));
+            _tabbedWindowTabControlFieldInfo = typeof(TabbedWindow).GetFields(BindingFlags.Instance | BindingFlags.NonPublic).Single(f => f.FieldType == typeof(TabControl));
         }
+
         public override string Author
         {
             get
@@ -43,6 +41,7 @@ namespace Faark.Gnomoria.Mods
                 return "Faark";
             }
         }
+
         public override string Description
         {
             get
@@ -51,8 +50,8 @@ namespace Faark.Gnomoria.Mods
             }
         }
 
-        private static Button CurrentPrevButton = null; 
-        private static Button CurrentNextButton = null;
+        private static Button _currentPrevButton; 
+        private static Button _currentNextButton;
 
         private static T GetPrev<T>(IEnumerable<T> list, T item )
         {
@@ -63,33 +62,35 @@ namespace Faark.Gnomoria.Mods
                 {
                     return currentPrev;
                 }
-                else
-                {
-                    currentPrev = cur;
-                }
+                
+                currentPrev = cur;
             }
             return default(T);
         }
+
         private static T GetNext<T>(IEnumerable<T> list,T item )
         {
             bool isMatch = false;
+
             foreach (var cur in list)
             {
                 if (isMatch)
                 {
                     return cur;
                 }
-                else if (EqualityComparer<T>.Default.Equals(cur, item))
+                
+                if (EqualityComparer<T>.Default.Equals(cur, item))
                 {
                     isMatch = true;
                 }
             }
             return default(T);
         }
+
         public static void ToggleTo(ViewCharacterUI old, Manager mgr, Character newChar)
         {
             var hud = GnomanEmpire.Instance.GuiManager.HUD;
-            var tc = (TabControl)TabbedWindow_TabControl_FieldInfo.GetValue(old);
+            var tc = (TabControl)_tabbedWindowTabControlFieldInfo.GetValue(old);
             var selPage = tc.SelectedIndex;
             if (hud.ActiveWindow == old)
             {
@@ -99,9 +100,10 @@ namespace Faark.Gnomoria.Mods
             var newWindow = new ViewCharacterUI(mgr, newChar);
             GnomanEmpire.Instance.GuiManager.HUD.ShowWindow(newWindow);
             newWindow.Focused = true;
-            var newTc = (TabControl)TabbedWindow_TabControl_FieldInfo.GetValue(newWindow);
+            var newTc = (TabControl)_tabbedWindowTabControlFieldInfo.GetValue(newWindow);
             newTc.SelectedIndex = selPage;
         }
+
         public static void OnCreate_ViewCharacterUI(ViewCharacterUI self, Manager mgr, Character chr)
         {
             var chrList = GnomanEmpire.Instance.World.AIDirector.PlayerFaction.Members.Select( m => m.Value ).OrderBy( m => m.Mind.Profession.Title );
@@ -113,22 +115,23 @@ namespace Faark.Gnomoria.Mods
             var centerWindow = self.ClientWidth / 2;
 
             var next = GetNext(chrList, chr);
-            var buttonNext = CurrentNextButton = new Button(mgr);
+            var buttonNext = _currentNextButton = new Button(mgr);
             buttonNext.Init();
             buttonNext.Text = "N";
             buttonNext.Width = 30;
             buttonNext.Anchor = Anchors.Bottom;
             buttonNext.Left = centerWindow + 3;
             buttonNext.Top = self.ClientHeight - buttonNext.Height - 3;
-            buttonNext.Click += new Game.GUI.Controls.EventHandler((sender, args) =>
+            buttonNext.Click += (sender, args) =>
             {
                 args.Handled = true;
                 ToggleTo(self, mgr, next);
-                if (CurrentNextButton.Enabled)
+                if (_currentNextButton.Enabled)
                 {
-                    CurrentNextButton.Focused = true;
+                    _currentNextButton.Focused = true;
                 }
-            });
+            };
+
             if (next == null)
             {
                 buttonNext.Enabled = false;
@@ -139,22 +142,25 @@ namespace Faark.Gnomoria.Mods
             }
 
             var prev = GetPrev(chrList, chr);
-            var buttonPrev = CurrentPrevButton = new Button(mgr);
+            var buttonPrev = _currentPrevButton = new Button(mgr);
             buttonPrev.Init();
             buttonPrev.Text = "P";
             buttonPrev.Width = 30;
             buttonPrev.Anchor = Anchors.Bottom;
             buttonPrev.Top = self.ClientHeight - buttonPrev.Height - 3;
             buttonPrev.Left = centerWindow - buttonPrev.Width - 3;
-            buttonPrev.Click += new Game.GUI.Controls.EventHandler((sender, args) =>
+            
+            // Todo: Find out why resharper complains about the implicit closure
+            buttonPrev.Click += (sender, args) =>
             {
                 args.Handled = true;
                 ToggleTo(self, mgr, prev);
-                if (CurrentPrevButton.Enabled)
+                if (_currentPrevButton.Enabled)
                 {
-                    CurrentPrevButton.Focused = true;
+                    _currentPrevButton.Focused = true;
                 }
-            });
+            };
+
             if (prev == null)
             {
                 buttonPrev.Enabled = false;
@@ -163,17 +169,16 @@ namespace Faark.Gnomoria.Mods
             {
                 buttonPrev.ToolTip = new ToolTip(mgr) { Text = "Prev: " + prev.NameAndTitle() };
             }
-            self.Closed += new WindowClosedEventHandler((sender, args) =>
+            self.Closed += (sender, args) =>
             {
-                if (CurrentNextButton == buttonNext)
-                    CurrentNextButton = null;
-                if (CurrentPrevButton == buttonPrev)
-                    CurrentPrevButton = null;
-            });
+                if (_currentNextButton == buttonNext)
+                    _currentNextButton = null;
+                if (_currentPrevButton == buttonPrev)
+                    _currentPrevButton = null;
+            };
 
             self.Add(buttonNext);
             self.Add(buttonPrev);
-
         }
     }
 #endif
