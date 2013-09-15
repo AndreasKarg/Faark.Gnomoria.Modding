7f647313ba6363fd79a0870873648f843b1d2d55
AndreasKarg <Clonkman@gmx.de>
2013-09-15 12:00:07 +0200

    Cleanup: Superficial refactoring of AutoBackup.cs

--- FaarksMods/Done/AutoBackup.cs
+++ FaarksMods/Done/AutoBackup.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Reflection;
 using Faark.Gnomoria.Modding;
 
 namespace Faark.Gnomoria.Mods
@@ -14,6 +13,8 @@ namespace Faark.Gnomoria.Mods
     /// </summary>
     public class Game_CreateBackupSavegame : Mod
     {
+        // Todo: Maybe move this class to Game-XX namespace?
+
         public override IEnumerable<IModification> Modifications
         {
             get
@@ -26,21 +27,22 @@ namespace Faark.Gnomoria.Mods
 
                 // there are 2 ways to get our method handles. Lets start with using System.Reflection. In that case it is easy,
                 // since there is just one SaveGame that is public. Otherwise you would may have to do other stuff, may search in .GetMethods(flags)
-                var original_function = typeof(Game.GnomanEmpire).GetMethod("SaveGame");
+                var originalFunction = typeof(Game.GnomanEmpire).GetMethod("SaveGame");
 
                 // But for public static functions, such as all of ours, you should use the following little helper. 
                 // The huge advantage: Compile time errors when e.g. renaming the method or just a spelling mistake
-                var our_function = Method.Of<Task, Game.GnomanEmpire, bool, Task>(OnAfter_SaveGame_Started);
+                var ourFunction = Method.Of<Task, Game.GnomanEmpire, bool, Task>(OnAfter_SaveGame_Started);
 
                 // and finally we want to return the list of all hooks (currently only one)
                 return new IModification[]{
                     new MethodHook(
-                        original_function,
-                        our_function
+                        originalFunction,
+                        ourFunction
                     )
                 };
             }
         }
+
         // A little effort to make your mod look nice in the mod list.
         public override string Author
         {
@@ -49,45 +51,50 @@ namespace Faark.Gnomoria.Mods
                 return "Faark";
             }
         }
+
         public override string Description
         {
             get
             {
-                return "A custom FPS counter. It was planned to add more metrics and/or convert it into a graph, but never got around to do it.";
+                return "Automatic backup of game saves.";
             }
         }
-        public static System.Threading.Tasks.Task OnAfter_SaveGame_Started(System.Threading.Tasks.Task save_task, Game.GnomanEmpire self, bool fallenKingdom)
+
+        public static Task OnAfter_SaveGame_Started(Task saveTask, Game.GnomanEmpire self, bool fallenKingdom)
         {
             // This function is hooked into the game as specified by GetConfig().
             // Be careful to exactly match the required structure (see other doc, sth like [RetVal|void]([retVal][this][arguments]) )
             // Currently there isn't any flexibility, so you have to specifiy all that apply
-            return save_task.ContinueWith((task) =>
+            return saveTask.ContinueWith((task) =>
             {
                 try
                 {
                     //scaning for the newest dated looks like the easies way to get the latest save, since i havent found any "get current save file"-func
-                    var latest_save_file = new System.IO.DirectoryInfo(Game.GnomanEmpire.SaveFolderPath("Worlds\\"))
+                    var latestSaveFile = new System.IO.DirectoryInfo(Game.GnomanEmpire.SaveFolderPath("Worlds\\"))
                         .GetFiles()
                         .Where(file => file.Extension.ToUpper() == ".SAV")
                         .Aggregate((a, b) =>
                         {
                             return a.LastWriteTime > b.LastWriteTime ? a : b;
                         });
-                    var backup_folder = System.IO.Path.Combine(latest_save_file.DirectoryName, "Backups");
-                    System.IO.Directory.CreateDirectory(backup_folder);
+
+                    Debug.Assert(latestSaveFile.DirectoryName != null, "latestSaveFile.DirectoryName != null");
+
+                    var backupFolder = System.IO.Path.Combine(latestSaveFile.DirectoryName, "Backups");
+                    System.IO.Directory.CreateDirectory(backupFolder);
                     System.IO.File.Copy(
-                        latest_save_file.FullName,
+                        latestSaveFile.FullName,
                         System.IO.Path.Combine(
-                            backup_folder,
-                            latest_save_file.Name.Remove(latest_save_file.Name.Length - latest_save_file.Extension.Length)
+                            backupFolder,
+                            latestSaveFile.Name.Remove(latestSaveFile.Name.Length - latestSaveFile.Extension.Length)
                             + "_" + DateTime.Now.ToString("yyyyMMddHHmmss")
-                            + latest_save_file.Extension
+                            + latestSaveFile.Extension
                             )
                         );
-                    return;
                 }
                 catch (Exception err)
                 {
+                    // Todo: Do something meaningful here...
                     RuntimeModController.Log.Write(err);
                 }
             });
