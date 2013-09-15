fb53f2018002bfb1a3d5ae674d6b9e5abcef0dbc
AndreasKarg <Clonkman@gmx.de>
2013-09-15 12:06:13 +0200

    Cleanup: Superficial refactoring of ExitWithoutSaveInMainMenu.cs

--- FaarksMods/Done/ExitWithoutSaveInMainMenu.cs
+++ FaarksMods/Done/ExitWithoutSaveInMainMenu.cs
@@ -1,16 +1,11 @@
-﻿ using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Reflection;
-using System.Runtime.Serialization;
 using Faark.Gnomoria.Modding;
 using Game;
 using Game.GUI;
 using Game.GUI.Controls;
-using GameLibrary;
-using Faark.Util;
-using Microsoft.Xna.Framework;
 
 namespace Faark.Gnomoria.Mods
 {
@@ -30,6 +25,7 @@ namespace Faark.Gnomoria.Mods
                     );
             }
         }
+
         public override string Author
         {
             get
@@ -37,6 +33,7 @@ namespace Faark.Gnomoria.Mods
                 return "Faark";
             }
         }
+
         public override string Description
         {
             get
@@ -44,30 +41,31 @@ namespace Faark.Gnomoria.Mods
                 return "This mod allows players to leave the current world without saving";
             }
         }
+
         public static void OnCreate_PauseMenu(PauseMenu self, Manager mgr)
         {
-            if (!GnomanEmpire.Instance.IsGameOver())
-            {
-                var panel = typeof(PauseMenu)
-                    .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
-                    .Single(f => f.FieldType == typeof(Panel))
-                    .GetValue(self)
-                    as Panel;
-                var lastBtn = panel.Controls.Last();
-                Button button = new Button(mgr);
-                button.Init();
-                button.Width = 200;
-                button.Top = lastBtn.Top + lastBtn.Height + lastBtn.Margins.Bottom + button.Margins.Top - lastBtn.Margins.Top; //yea, lastMarginTop is excluded between save buttons
-                button.Left = (panel.Width - button.Width) / 2;
-                //button.Margins = new Margins(0, 2, 0, 2);
-                button.Text = "Exit (no Save)";
-                button.Click += new Game.GUI.Controls.EventHandler((sender, args) =>
-                {
-                    GnomanEmpire.Instance.MoveToMainMenu();
-                });
-                panel.Height = button.Top + button.Height;
-                panel.Add(button);
-            }
+            if (GnomanEmpire.Instance.IsGameOver()) return;
+
+            var panel = typeof(PauseMenu)
+                .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
+                .Single(f => f.FieldType == typeof(Panel))
+                .GetValue(self)
+                as Panel;
+
+            Debug.Assert(panel != null, "panel != null");
+
+            var lastBtn = panel.Controls.Last();
+            var button = new Button(mgr);
+
+            button.Init();
+            button.Width = 200;
+            button.Top = lastBtn.Top + lastBtn.Height + lastBtn.Margins.Bottom + button.Margins.Top - lastBtn.Margins.Top; //yea, lastMarginTop is excluded between save buttons
+            button.Left = (panel.Width - button.Width) / 2;
+            //button.Margins = new Margins(0, 2, 0, 2);
+            button.Text = "Exit (no Save)";
+            button.Click += (sender, args) => GnomanEmpire.Instance.MoveToMainMenu();
+            panel.Height = button.Top + button.Height;
+            panel.Add(button);
         }
     }
 #endif
